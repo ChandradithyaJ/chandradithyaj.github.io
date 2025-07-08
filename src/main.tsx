6e29ba91847f2f10cfd97071a19f43e413fc1195
Chandradithya <chandradithya@gmail.com>
2025-07-08 23:45:31 +0530

    setting up gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 createRoot(document.getElementById("root")!).render(<App />);
